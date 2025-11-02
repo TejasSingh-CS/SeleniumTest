97447ba1c698d6e367416cd0fb4f2c87f3152916
Tejas Singh <tejas.singh@confidosoft.co.uk>
2025-11-02 20:52:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://google.com");
+            driver.Manage().Window.Maximize();
+            IWebElement googleSearchBox = driver.FindElement(By.Name("q"));
+            googleSearchBox.SendKeys("Amazon");
+            googleSearchBox.SendKeys(Keys.Enter);
+        }
+    }
+}
